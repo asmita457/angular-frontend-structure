f86fc8264d81cd0f2ce70d5c3c839d73e6181e5d
DhananjayRaut <asmita.belhekar@gmail.com>
2020-06-13 15:50:45 +0530

    new changes

--- src/app/components/newproduct/productcreate/productcreate.component.ts
+++ src/app/components/newproduct/productcreate/productcreate.component.ts
@@ -12,7 +12,7 @@ import { environment } from 'src/environments/environment';
 })
 export class ProductcreateComponent implements OnInit, CreateUpdateComponentInterface {
 
-  resourceName = "products";
+  resourceName = "product";
   baseUrl: string = "";
   id;
   data;
